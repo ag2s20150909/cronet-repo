6a9fe267a2ffe0a6180e293cb646d3331f6a2a02
ag2s20150909 <ag2s20150909@gmail.com>
2021-07-24 04:48:19 +0800

    md5

--- cronet/filemd5.py
+++ cronet/filemd5.py
@@ -12,7 +12,7 @@ def getFileList():
                 fs.append(os.path.join(root,file))
     return fs
 def saveMD5(path):
-    file=open(path+".md5",mode='w',encoding='utf-8')
+    file=open(path+".js",mode='w',encoding='utf-8')
     file.write(hashlib.md5(open(path,'rb').read()).hexdigest())
     file.flush()
     file.close()
