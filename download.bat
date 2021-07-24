9d8417f0639145baac5d0354abdc90c798402cc5
ag2s20150909 <ag2s20150909@gmail.com>
2021-07-24 18:50:13 +0800

    Tiny the git and update cronet 92.0.4515.98

---
+++ download.bat
@@ -0,0 +1,35 @@
+@echo off
+::set Cronet version
+SET CronetVersion="92.0.4515.98"
+
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/cronet_api.jar" ./cronet/%CronetVersion%/libs/cronet_api.jar
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/cronet_impl_common_java.jar" ./cronet/%CronetVersion%/libs/cronet_impl_common_java.jar
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/cronet_impl_native_java.jar" ./cronet/%CronetVersion%/libs/cronet_impl_native_java.jar
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/cronet_impl_platform_java.jar" ./cronet/%CronetVersion%/libs/cronet_impl_platform_java.jar
+
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/libs/arm64-v8a/libcronet.%CronetVersion%.so" ./cronet/%CronetVersion%/arm64-v8a/libcronet.%CronetVersion%.so
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/libs/armeabi-v7a/libcronet.%CronetVersion%.so" ./cronet/%CronetVersion%/armeabi-v7a/libcronet.%CronetVersion%.so
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/libs/x86_64/libcronet.%CronetVersion%.so" ./cronet/%CronetVersion%/x86_64/libcronet.%CronetVersion%.so
+gsutil -m cp "gs://chromium-cronet/android/%CronetVersion%/Release/cronet/libs/x86/libcronet.%CronetVersion%.so" ./cronet/%CronetVersion%/x86/libcronet.%CronetVersion%.so
+
+python filemd5.py
+
+echo "创建并切入分支 latest_branch"
+git checkout --orphan latest_branch
+
+echo "添加所有文件"
+git add -A
+
+echo "提交改变"
+git commit -am "Tiny the git and update cronet %CronetVersion%"
+
+echo "删除 main 分支"
+git branch -D main
+
+echo "重命名当前分支为 main"
+git branch -m main
+
+echo "强制更新到远程仓库"
+git push -f origin main
+
+pause
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Cronet repo
+
+Cronet binary files are from [Cronet Google Cloud Platform](https://console.cloud.google.com/storage/browser/chromium-cronet/android)
