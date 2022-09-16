24a46fded5bf8f5bbfced7d4c684d6b1e480554d
ag2s20150909 <ag2s20150909@gmail.com>
2022-09-16 11:28:01 +0800

    test

---
+++ library/okhttp/src/main/java/me/ag2s/cronet/okhttp/CronetCallBackNew.java
@@ -0,0 +1,68 @@
+package me.ag2s.cronet.okhttp;
+
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import org.chromium.net.UrlRequest;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+class CronetCallBackNew extends AbsCronetCallback {
+
+    private final CompletableFuture<Response> responseFuture = new CompletableFuture<>();
+
+
+    CronetCallBackNew(Request request, Call call) {
+        super(request, call);
+    }
+
+
+    @Override
+    Response waitForDone(@NonNull UrlRequest urlRequest) throws IOException {
+        try {
+            if (mCall.timeout().timeoutNanos() > 0) {
+                return responseFuture.get(mCall.timeout().timeoutNanos(), TimeUnit.NANOSECONDS);
+            } else {
+                return responseFuture.get();
+            }
+
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            throw new IOException(e.getMessage(), e.getCause());
+        }
+    }
+
+    /**
+     * 请求成功后，通知子类结束阻塞，返回response
+     *
+     * @param response Response
+     */
+    @Override
+    void onSuccess(@NonNull Response response) {
+        responseFuture.complete(response);
+    }
+
+    /**
+     * 当发生错误时，通知子类终止阻塞抛出错误
+     *
+     * @param error IOException
+     */
+    @Override
+    void onError(@NonNull IOException error) {
+        responseFuture.completeExceptionally(error);
+    }
+
+
+
+}
---
+++ library/okhttp/src/main/java/me/ag2s/cronet/okhttp/CronetCallBackOld.java
@@ -0,0 +1,68 @@
+package me.ag2s.cronet.okhttp;
+
+import android.os.ConditionVariable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.chromium.net.UrlRequest;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Request;
+import okhttp3.Response;
+
+class CronetCallBackOld extends AbsCronetCallback {
+    private final ConditionVariable mResponseCondition = new ConditionVariable();
+    @Nullable
+    private IOException mException;
+
+    CronetCallBackOld(Request request, Call call) {
+        super(request, call);
+    }
+
+    @Override
+    Response waitForDone(@NonNull UrlRequest urlRequest) throws IOException {
+
+        long timeOutMs = mCall.timeout().timeoutNanos() / 1000000;
+        //Log.e(TAG, "timeout:" + timeOutMs);
+        if (timeOutMs > 0) {
+            mResponseCondition.block(timeOutMs);
+        } else {
+            mResponseCondition.block();
+        }
+        if (!urlRequest.isDone()) {
+            urlRequest.cancel();
+            mException = new IOException("time out");
+        }
+
+
+        if (mException != null) {
+            throw mException;
+        }
+
+        return mResponse;
+    }
+
+    /**
+     * 请求成功后，通知子类结束阻塞，返回response
+     *
+     * @param response Response
+     */
+    @Override
+    void onSuccess(@NonNull Response response) {
+        mResponseCondition.open();
+    }
+
+    /**
+     * 当发生错误时，通知子类终止阻塞抛出错误
+     *
+     * @param error IOException
+     */
+    @Override
+    void onError(@NonNull IOException error) {
+        mException = error;
+        mResponseCondition.open();
+    }
+}
