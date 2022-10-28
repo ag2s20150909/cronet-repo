dfae462e3856860b0048d71bcb938e8e2f053607
ag2s20150909 <ag2s20150909@gmail.com>
2022-10-28 22:11:07 +0800

    update

--- .github/scripts/cronetStable.sh
+++ .github/scripts/cronetStable.sh
@@ -1,7 +1,7 @@
 #bin/sh
 echo "fetch release info from https://chromiumdash.appspot.com ..."
 
-branch="Beta"
+branch="Stable"
 
 lastest_cronet_version=`curl -s "https://chromiumdash.appspot.com/fetch_releases?channel=$branch&platform=Android&num=1&offset=0" | jq .[0].version -r`
 echo "lastest_cronet_version: $lastest_cronet_version"
@@ -16,6 +16,7 @@ if [[  $current_cronet_version == $lastest_cronet_version ]];then
     echo "cronet is already latest"
 else
     sed -i s/CronetVersion=.*/CronetVersion=$lastest_cronet_version/ $path
+    sed -i s/PROJ_VERSION=.*/PROJ_VERSION=$lastest_cronet_version/ $path
     #sed "15a* 更新cronet: $lastest_cronet_version" -i $GITHUB_WORKSPACE/app/src/main/assets/updateLog.md
     echo "start download latest cronet"
     chmod +x gradlew
