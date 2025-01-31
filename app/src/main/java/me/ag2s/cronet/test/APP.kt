d4f16fe97a3c8f51696f467621f7c2c15be0e743
ag2s20150909 <19373730+ag2s20150909@users.noreply.github.com>
2025-01-31 15:18:27 +0800

    up

--- app/src/main/java/me/ag2s/cronet/test/APP.kt
+++ app/src/main/java/me/ag2s/cronet/test/APP.kt
@@ -2,10 +2,20 @@ package me.ag2s.cronet.test
 
 import android.app.Application
 import android.content.Context
+import android.os.Build.VERSION.SDK_INT
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.gif.AnimatedImageDecoder
+import coil3.gif.GifDecoder
+import coil3.memory.MemoryCache
+import coil3.request.crossfade
+import okio.Path.Companion.toOkioPath
 
 lateinit var appCtx: APP
 
-class APP : Application() {
+class APP : Application(),SingletonImageLoader.Factory {
     override fun onCreate() {
         super.onCreate()
         appCtx = this
@@ -15,6 +25,30 @@ class APP : Application() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         appCtx = this
+    }
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .crossfade(true)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.externalCacheDir!!.resolve("ktor").toOkioPath())
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .components {
+                if (SDK_INT >= 28) {
+                    add(AnimatedImageDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
+            .build()
     }
 
 
--- app/src/main/java/me/ag2s/cronet/test/NetImage.kt
+++ app/src/main/java/me/ag2s/cronet/test/NetImage.kt
@@ -4,12 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.load.model.GlideUrl
+import coil3.compose.AsyncImage
 
 
-@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun NetImage(
     model: Any?,
@@ -18,14 +15,16 @@ fun NetImage(
     contentScale: ContentScale = ContentScale.FillWidth,
 ) {
 
-    GlideImage(
-        model = GlideUrl(model.toString()),
-        contentDescription = "",
-        modifier = modifier,
-        alignment = alignment,
-        contentScale = contentScale,
+    AsyncImage(model, contentDescription = "", modifier = modifier,alignment=alignment, contentScale = contentScale)
 
-        )
+//    GlideImage(
+//        model = GlideUrl(model.toString()),
+//        contentDescription = "",
+//        modifier = modifier,
+//        alignment = alignment,
+//        contentScale = contentScale,
+//
+//        )
 
 
 }
